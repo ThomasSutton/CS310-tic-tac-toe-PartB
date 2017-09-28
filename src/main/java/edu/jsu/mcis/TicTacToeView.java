5122864f7d1c624307ee6e25fbdab294bdfc9685
Adam Sutton <tsutton1@stu.jsu.edu>
2017-09-27 21:21:46 -0500

    Implemented new TicTacToeView class

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,72 +1,92 @@
 package edu.jsu.mcis;
 
-public class TicTacToeView {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-    private TicTacToeModel model;
+public class TicTacToeView extends JPanel implements ActionListener {
     
-    /* CONSTRUCTOR */
-	
+    TicTacToeModel model;
+
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+
     public TicTacToeView(TicTacToeModel model) {
-        
+
         this.model = model;
+
+        int width = model.getWidth();
+
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        /* Loop through all rows and columns of the grid and: create a new
+           JButton and assign it to the current grid element, add "this"
+           (the View object) as the ActionListener, set the JButton's name
+           to "SquareXY" (where X is the row and Y is the column), set the
+           preferred size to 64x64 pixels, and add the new square to the
+           "squaresPanel" JPanel created earlier. */
+        
+        /* INSERT CODE HERE */
+        
+        add(squaresPanel);
+        add(resultLabel);
+        
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        String name = ((JButton) event.getSource()).getName();
+        
+        /* Extract the ROW and COL from the string name (remember, the squares
+           should all be named "SquareXY", so the ROW should be extracted from
+           position 6 of this string, and the COL from position 7.  Remember to
+           convert both to Integers!) */
+
+        /* INSERT CODE HERE */
+           
+        /* Call makeMark() to place the mark in the Model */
+        
+        /* INSERT CODE HERE */
+
+        /* Update the squares of the View using the "updateSquares()" method
+           (see below); this is the equivalent of re-printing the grid to the
+           console in Part One. */
+
+        updateSquares();
+
+        /* Clear the "showResult" label at the bottom of the View */
+        
+        showResult(" ");
+        
+        /* If the game is over (that is, if the "getResult()" method returns
+           a result other than Result.NONE), loop through all the JButtons in
+           the View and disable them to prevent further entries.  (Hint: the
+           JButton class provides a "setEnabled()" method for this.) */
+           
+        /* INSERT CODE HERE */
         
     }
-	
-    public void viewModel() {
         
-        /* Print the board to the console (see examples) */
+    public void updateSquares() {
+
+        /* Loop through all of the rows and columns in the JButton grid and
+           reset the button text to match the corresponding marks as returned
+           by "getMark()" (to update the View to match the Model) */
+           
+        /* INSERT CODE HERE */
+
+    }
         
-        /* INSERT YOUR CODE HERE */
-        System.out.print("\n\n");
-        System.out.print("  ");
-        for (int p = 0; p < model.getWidth(); p++) {
-            System.out.print(p);
-        }
-        System.out.print("\n");
-        for (int i = 0; i < model.getWidth(); i++) {
-            System.out.print(i + " ");
-            for (int j = 0; j < model.getWidth(); j++){
-               System.out.print(model.getMark(i, j));
-            }
-            System.out.print("\n");
-        }
-        System.out.print("\n\n");
-              
-        
-
+    public void showResult(String message) {
+        resultLabel.setText(message);
     }
 
-    public void showNextMovePrompt() {
-
-        /* Display a prompt for the player's next move (see examples) */
-
-        /* INSERT YOUR CODE HERE */
-        if (model.isXTurn()){
-            System.out.println("Player 1 (X) Move:\n" +
-            "Enter the row and column numbers, separated by a space: ");
-        }
-        else {
-            System.out.println("Player 2 (O) Move:\n" +
-            "Enter the row and column numbers, separated by a space: ");
-        }
-
-    }
-
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-
-        /* INSERT YOUR CODE HERE */
-        System.out.println("Invalid Input");
-
-    }
-
-    public void showResult(String r) {
-
-        /* Display final winner */
-
-        System.out.println(r + "!");
-
-    }
-	
 }
